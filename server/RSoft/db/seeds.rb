9adcaa5b415d069f273159a4282f49dc8ca51496
Alexey Manoilo <mamanuch@mail.ru>
2013-12-19 00:51:18 +0200

    include all modules,

--- server/RSoft/db/seeds.rb
+++ server/RSoft/db/seeds.rb
@@ -19,5 +19,15 @@
     Table.create([{orderid:'nonee',state:'vacant',activity:'false',capacity:'3',waiter:'n/a'}])
     Table.create([{orderid:'nonee',state:'vacant',activity:'false',capacity:'5',waiter:'n/a'}])
 
+    MenuItem.create([{category:'Drincks',name:'wine',description:'white add other descriptions',price:40}])
+    MenuItem.create([{category:'Drincks',name:'wine',description:'white add other descriptions',price:50}])   
+    MenuItem.create([{category:'Desserts',name:'tiramisu',description:'Desserts descriptions',price:20}])
+    MenuItem.create([{category:'Desserts',name:'pudding',description:'Desserts descriptions',price:30}])
+    MenuItem.create([{category:'Sides',name:'some Side',description:'Sides descriptions',price:15}])
+    MenuItem.create([{category:'Sides',name:'some Side2',description:'Sides descriptions',price:35}])
+    
+    
 
+
+    
 
