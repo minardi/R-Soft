a812169ed4fde0795de689c26e9d8309bc627d73
Anna Vostrykova <it4anna@gmail.com>
2013-12-23 09:13:17 +0200

    +MenuItemDescription(client side)

---
+++ client/test/collections/MenuItemDesc.spec.js
@@ -0,0 +1,10 @@
+/*global describe, beforeEach, assert, it  */
+'use strict';
+
+describe('Menuitemdesc Collection', function () {
+
+    beforeEach(function () {
+        this.Menuitemdesc = new client.Collections.MenuitemdescCollection();
+    });
+
+});
---
+++ client/test/views/MenuItemDesc.spec.js
@@ -0,0 +1,10 @@
+/*global describe, beforeEach, assert, it  */
+'use strict';
+
+describe('Menuitemdesc View', function () {
+
+    beforeEach(function () {
+        this.Menuitemdesc = new client.Views.MenuitemdescView();
+    });
+
+});
