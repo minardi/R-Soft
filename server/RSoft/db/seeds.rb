d8515b9958d7ba8d50e1d35dfe285b4c54f838e9
D Petin <temp_email@inbox.ru>
2013-12-26 12:42:45 +0200

    commit nenygnij

--- server/RSoft/db/seeds.rb
+++ server/RSoft/db/seeds.rb
@@ -25,14 +25,6 @@
     MenuItem.create([{category:'Desserts',name:'Apple Pie',description:'4',price:30}])
     MenuItem.create([{category:'Sides',name:'Cheese',description:'5',price:15}])
     MenuItem.create([{category:'Bar',name:'Green Mexican',description:'6',price:35}])
-	
-	
-	MenuItemDesc.create([{name:'Capuccino', description:'Frothed milk and a shot of espresso, topped with a touch of cinnamon', uri:1.jpg}])
-	MenuItemDesc.create([{name:'Esspreso', description:'A type of coffee prepared from a blend of coffee beans and produced when pressed through a fine filter under high pressure.', uri:2.jpg}])
-	MenuItemDesc.create([{name:'Ice Cream', description:'Our delicious traditional vanilla ice cream with a chocolate fudge sauce rippled throughout.', uri:3.jpg}])
-	MenuItemDesc.create([{name:'Apple Pie', description:'Using fresh apples, a homemade pie crust recipe, and the perfect blend of spices; this is the apple pie that is true delight in every forkful.', uri:4.jpg}])
-	MenuItemDesc.create([{name:'Cheese', description:'Some description, some description, ect..', uri:5.jpg}])
-	MenuItemDesc.create([{name:'Green Mexican', description:'Some coctails description', uri:6.jpg}])
 
     waiter = User.new
     waiter.email = 'waiter@rsoft.ru'
